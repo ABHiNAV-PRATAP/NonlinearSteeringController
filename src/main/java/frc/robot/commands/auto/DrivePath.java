d6e3f0d8f558202d173721427c3769c454a39641
Joydeep Mukherjee <joydeepm02@gmail.com>
2019-01-26 15:19:36 +0000

    Update DrivePath.java

--- src/main/java/frc/robot/commands/auto/DrivePath.java
+++ src/main/java/frc/robot/commands/auto/DrivePath.java
@@ -19,64 +19,45 @@ public class DrivePath extends Command {
   /**
    * General command to drive a trajectory.
    */
-  private Trajectory leftTrajectory, rightTrajectory;
+  private Trajectory trajectory;
 
   private RamseteFollower ramseteFollower;
 
-  public DrivePath(Trajectory l, Trajectory r) {
-    System.out.println("AutoDrive started");
-    
-    leftTrajectory = l;
-    rightTrajectory = r;
-
-    leftFollower = new EncoderFollower(leftTrajectory);
-    rightFollower = new EncoderFollower(rightTrajectory);
-
-    leftFollower.configureEncoder(RobotMap.leftDriveEncoder.get(), RobotConstants.kDriveEncoderTicksPerRev, RobotConstants.kWheelDiameter);
-    rightFollower.configureEncoder(RobotMap.rightDriveEncoder.get(), RobotConstants.kDriveEncoderTicksPerRev, RobotConstants.kWheelDiameter);
-    leftFollower.configurePIDVA(RobotConstants.kP, RobotConstants.kI, RobotConstants.kD, 1 / RobotConstants.kVMax, RobotConstants.kA);
-    rightFollower.configurePIDVA(RobotConstants.kP, RobotConstants.kI, RobotConstants.kD, 1 / RobotConstants.kVMax, RobotConstants.kA);
-
+  public DrivePath(Trajectory trajectory) {
+    this.trajectory = trajectory;
     requires(Robot.drivetrain);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    ramseteFollower = new RamseteFollower(trajectory);
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    double leftSpeed = leftFollower.calculate(-RobotMap.leftDriveEncoder.get());
-    double rightSpeed = rightFollower.calculate(RobotMap.rightDriveEncoder.get());
-    
-    double gyroHeading = -RobotMap.gyro.getAngle();
-    double desiredHeading = Pathfinder.r2d(leftFollower.getHeading());
-
-    double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
-    double turn = 0.8 * (-1.0 / 80.0) * angleDifference;
-
-    // System.out.println("Turn value: " + turn);
-    // System.out.println("Gyro value: " + RobotMap.gyro.getAngle());
-    // System.out.println("Left: " + leftSpeed);
-    // System.out.println("Right: " + rightSpeed);
-    // System.out.println("Left Encoder: " + (-RobotMap.leftDriveEncoder.get()));
-    // System.out.println("Right Encoder: " + RobotMap.rightDriveEncoder.get());
-    Robot.drivetrain.drive(leftSpeed+turn, rightSpeed-turn);
+    // Update odometry
+    ramseteFollower.o.updateOdometry();
+    // Calculate linear and angular velocities
+    double v = ramseteFollower.calculateLinearVelocity();
+    double w = ramseteFollower.calculateAngularVelocity();
+    // Set linear and angular velocities to drivetrain
+    double l = (-1 * (w * RobotConstants.kWheelBase) + (2 * v)) / 2;
+    double r = ((w * RobotConstants.kWheelBase) + (2 * v)) / 2;
+    Robot.drivetrain.drive(l, r);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return leftFollower.isFinished() && rightFollower.isFinished();
+    return ramseteFollower.isFinished();
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
     Robot.drivetrain.stop();
-    System.out.println("AutoDrive finished");
   }
 
   // Called when another command which requires one or more of the same
